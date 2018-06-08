a53867ec1765dbf8fe95ae081af7bc7b47a627bc
Jason Sanchez <sanchezjason@me.com>
2018-06-08 14:59:26 -0600

    Physics Implementation

---
+++ PaperToss/PaperToss/GameScene.swift
@@ -0,0 +1,223 @@
+//
+//  GameScene.swift
+//  PaperToss
+//
+//  Created by Jason Sanchez on 6/8/18.
+//  Copyright © 2018 Jason Sanchez. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum GameState {
+    case playing
+    case menu
+    static var current = GameState.playing
+}
+
+// Physics Category
+struct pc {
+    static let none: UInt32 = 0x1 << 0
+    static let ball: UInt32 = 0x1 << 1
+    static let lBin: UInt32 = 0x1 << 2
+    static let rBin: UInt32 = 0x1 << 3
+    static let base: UInt32 = 0x1 << 4
+    static let sG: UInt32 = 0x1 << 5
+    static let eG: UInt32 = 0x1 << 6
+}
+
+// Statrt and End Touch points
+struct t {
+    static var start = CGPoint()
+    static var end = CGPoint()
+}
+
+// Constatnts
+struct c {
+    static var grav = CGFloat()  // Gravity
+    static var yVel = CGFloat()  // Initial y Velocity
+    static var airTime = TimeInterval() // Time the ball is in the air
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    // Variables
+    
+    var grids = true
+    
+    var bg = SKSpriteNode(imageNamed: "bgImage")
+    var bBack = SKSpriteNode(imageNamed: "binBack")
+    var bFront = SKSpriteNode(imageNamed: "binFront")
+    var pBall = SKSpriteNode(imageNamed: "paperBallImage")
+    
+    
+    var startG = SKShapeNode()  // Where the paper ball will start
+    var endG = SKShapeNode()    // The ground that the bin will sit on
+    var leftWall = SKShapeNode()
+    var rightWall = SKShapeNode()
+    var base = SKShapeNode()
+    var ball = SKShapeNode()
+    
+    
+    var windLbl = SKLabelNode()
+    
+    var pi = CGFloat(Double.pi)
+    var wind = CGFloat()
+    
+    override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            c.grav = -6
+            c.yVel = self.frame.height / 4
+            c.airTime = 2
+        } else {
+            // iPad
+        }
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: c.grav)
+        
+        setUpGame()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if GameState.current == .playing {
+                if ball.contains(location) {
+                    t.start = location
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if GameState.current == .playing && !ball.contains(location) {
+                t.end = location
+                fire()
+            }
+        }
+    }
+    
+    func setUpGame() {
+        GameState.current = .playing
+        
+        let bgScale = CGFloat(bg.frame.width / bg.frame.height) // eg. 1.4 as a scale
+        
+        bg.size.height = self.frame.height
+        bg.size.width = bg.size.height * bgScale
+        bg.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        bg.zPosition = 0
+        
+        self.addChild(bg)
+        
+        let binScale = CGFloat(bBack.frame.width / bBack.frame.height)
+        
+        bBack.size.height = self.frame.height / 9
+        bBack.size.width = bBack.size.height * binScale
+        bBack.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 3)
+        bBack.zPosition = bg.zPosition + 1
+        
+        self.addChild(bBack)
+        
+        bFront.size = bBack.size
+        bFront.position = bBack.position
+        bFront.zPosition = bBack.zPosition + 3
+        
+        self.addChild(bFront)
+        
+        startG = SKShapeNode(rectOf: CGSize(width: self.frame.width, height: 5))
+        startG.fillColor = .red
+        startG.strokeColor = .clear
+        startG.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 10)
+        startG.zPosition = 10
+        startG.alpha = grids ? 1 : 0 // True = 1 : Not True = 0
+        
+        startG.physicsBody = SKPhysicsBody(rectangleOf: startG.frame.size)
+        startG.physicsBody?.categoryBitMask = pc.sG
+        startG.physicsBody?.collisionBitMask = pc.ball
+        startG.physicsBody?.contactTestBitMask = pc.none
+        startG.physicsBody?.affectedByGravity = false
+        startG.physicsBody?.isDynamic = false
+        
+        self.addChild(startG)
+        
+        endG = SKShapeNode(rectOf: CGSize(width: self.frame.width * 2, height: 5))
+        endG.fillColor = .red
+        endG.strokeColor = .clear
+        endG.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 3 - bFront.frame.height / 2)
+        endG.zPosition = 10
+        endG.alpha = grids ? 1 : 0
+        
+        endG.physicsBody = SKPhysicsBody(rectangleOf: endG.frame.size)
+        endG.physicsBody?.categoryBitMask = pc.eG
+        endG.physicsBody?.collisionBitMask = pc.ball
+        endG.physicsBody?.contactTestBitMask = pc.none
+        endG.physicsBody?.affectedByGravity = false
+        endG.physicsBody?.isDynamic = false
+        
+        self.addChild(endG)
+        
+        leftWall = SKShapeNode(rectOf: CGSize(width: 3, height: bFront.frame.height / 1.6))
+        leftWall.fillColor = .red
+        leftWall.strokeColor = .clear
+        leftWall.position = CGPoint(x: bFront.position.x - bFront.frame.width / 2.5, y: bFront.position.y)
+        leftWall.zPosition = 10
+        leftWall.alpha = grids ? 1 : 0
+        
+        leftWall.physicsBody = SKPhysicsBody(rectangleOf: leftWall.frame.size)
+        leftWall.physicsBody?.categoryBitMask = pc.lBin
+        leftWall.physicsBody?.collisionBitMask = pc.ball
+        leftWall.physicsBody?.contactTestBitMask = pc.none
+        leftWall.physicsBody?.affectedByGravity = false
+        leftWall.physicsBody?.isDynamic = false
+        leftWall.zRotation = pi / 25
+        
+        self.addChild(leftWall)
+        
+        rightWall = SKShapeNode(rectOf: CGSize(width: 3, height: bFront.frame.height / 1.6))
+        rightWall.fillColor = .red
+        rightWall.strokeColor = .clear
+        rightWall.position = CGPoint(x: bFront.position.x + bFront.frame.width / 2.5, y: bFront.position.y)
+        rightWall.zPosition = 10
+        rightWall.alpha = grids ? 1 : 0
+        
+        rightWall.physicsBody = SKPhysicsBody(rectangleOf: rightWall.frame.size)
+        rightWall.physicsBody?.categoryBitMask = pc.rBin
+        rightWall.physicsBody?.collisionBitMask = pc.ball
+        rightWall.physicsBody?.contactTestBitMask = pc.none
+        rightWall.physicsBody?.affectedByGravity = false
+        rightWall.physicsBody?.isDynamic = false
+        rightWall.zRotation = -pi / 25
+        
+        self.addChild(rightWall)
+        
+        base = SKShapeNode(rectOf: CGSize(width: bFront.frame.width / 2, height: 3))
+        base.fillColor = .red
+        base.strokeColor = .clear
+        base.position = CGPoint(x: bFront.position.x, y: bFront.position.y - bFront.frame.height / 4)
+        base.zPosition = 10
+        base.alpha = grids ? 1 : 0
+        
+        base.physicsBody = SKPhysicsBody(rectangleOf: base.frame.size)
+        base.physicsBody?.categoryBitMask = pc.base
+        base.physicsBody?.collisionBitMask = pc.ball
+        base.physicsBody?.contactTestBitMask = pc.ball
+        base.physicsBody?.affectedByGravity = false
+        base.physicsBody?.isDynamic = false
+        
+        self.addChild(base)
+        
+        // 58:14 
+        
+        
+        
+        
+    }
+    
+    func fire() {
+        
+    }
+}
---
+++ PaperToss/PaperToss/GameViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GameViewController.swift
+//  PaperToss
+//
+//  Created by Jason Sanchez on 6/8/18.
+//  Copyright © 2018 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                scene.size = self.view.bounds.size
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .portrait
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
